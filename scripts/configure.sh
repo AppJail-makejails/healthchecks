013663837342e0b13d2269ff803aef5704dd055d
Jesús Daniel Colmenares Oviedo <DtxdF@disroot.org>
2025-08-04 16:56:30 -0400

    Add Makejails, Add files, Add script, Add templates

---
+++ scripts/configure.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+HCKS_DB="${HCKS_DB:-sqlite}"; export HCKS_DB
+HCKS_DB_NAME="${HCKS_DB_NAME:-/data/hc.sqlite}"; export HCKS_DB_NAME
+
+env | grep -Ee '^HCKS_.+=.*$' | cut -d= -f2- | while IFS= read -r env; do
+    env_key=`printf "%s" "${env}" | sed -Ee 's/^HCKS_([A-Za-z0-9_]+)=.*$/\1/'`
+    env_value=`printf "%s" "${env}" | cut -s -d "=" -f2-`
+
+    printf "env = %s=%s\n" "${env_key}" "${env_value}"
+done >> "/uwsgi.ini" || exit $?
